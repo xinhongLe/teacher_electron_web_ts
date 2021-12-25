52bd6ca622a65997fbc600940e8a1f9136436877
songwenlin <15706218382@163.com>
2021-12-25 15:00:26 +0800

    修复测评中心试卷下载问题

--- src/views/assessmentCenter/index.vue
+++ src/views/assessmentCenter/index.vue
@@ -1,22 +1,39 @@
 <template>
     <div class="container">
-        <webview v-if="isElectron" class="iframe" src="http://1.15.4.131/"></webview>
-        <iframe src="http://1.15.4.131/" sandbox="allow-forms allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation" class="iframe" v-else/>
+        <webview
+            v-if="isElectron"
+            class="iframe"
+            src="http://1.15.4.131/"
+        ></webview>
+        <iframe
+            src="http://1.15.4.131/"
+            sandbox="allow-forms allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"
+            class="iframe"
+            v-else
+        />
     </div>
 </template>
 
 <script lang="ts">
 import isElectron from "is-electron";
 import { defineComponent } from "vue";
-
-export default defineComponent(({
+// ts
+export default defineComponent({
     name: "assessmentCenter",
     setup() {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        window.onmessage = (e: any, d: any) => {
+            if (isElectron()) return;
+            if (e.data.url && e.data.download) {
+                location.href = e.data.url;
+            }
+        };
         return {
             isElectron: isElectron()
         };
     }
-}));
+});
 </script>
 
 <style lang="scss" scoped>
