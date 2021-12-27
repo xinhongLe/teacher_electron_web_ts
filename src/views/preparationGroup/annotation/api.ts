cf0fb8c1c9861b75142d37058b1032802f56449d
reget <jijie18465183529@163.com>
2021-12-27 18:00:10 +0800

    some

--- src/views/preparationGroup/annotation/api.ts
+++ src/views/preparationGroup/annotation/api.ts
@@ -1,4 +1,4 @@
-import { AI_XUE_SHI_API } from "@/config";
+import { WINDOW_CRAD_API } from "@/config";
 import request from "@/utils/request";
 import { IResponse } from "@/types/response";
 import { ICardList } from "@/types/intelligence";
@@ -10,7 +10,7 @@ type GetWindowCardsResponse = IResponse<ICardList[]>
 // 获取窗下的卡、页
 export function getWindowCards(data: IGetWindowCards): Promise<GetWindowCardsResponse> {
     return request({
-        baseURL: AI_XUE_SHI_API,
+        baseURL: WINDOW_CRAD_API,
         url: "API/W1/Card/GetWindowCards",
         headers: {
             "Content-Type": "application/json-patch+json"
