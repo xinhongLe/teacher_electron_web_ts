9f5b28759b830a42d39550f5e7feb4baeabf8915
songwenlin <15706218382@163.com>
2022-03-16 18:10:25 +0800

    --task=1002063 --user=宋文林 翻转课堂与数智课堂大融合（各模式互相调用方案）

--- src/hooks/useOpenWindow.ts
+++ src/hooks/useOpenWindow.ts
@@ -1,24 +1,36 @@
 import { ClassroomType, SchoolWindowInfo } from "@/types/preparation";
 import emitter from "@/utils/mitt";
-import { onMounted, onUnmounted, Ref } from "vue";
+import { onActivated, onDeactivated, onMounted, onUnmounted, Ref } from "vue";
 
 export default (tabIndex: Ref<ClassroomType>, updateCurrentWindow: (win: SchoolWindowInfo) => void, updateShowClassArrangement?: (flag: boolean) => void) => {
+    const openWindow: (event: Electron.IpcRendererEvent, ...args: any[]) => void = (_, data) => {
+        if (tabIndex.value === ClassroomType.Classes) {
+            tabIndex.value = ClassroomType.WindowClasses;
+        }
+        if (updateShowClassArrangement) {
+            updateShowClassArrangement(false);
+        }
+        emitter.emit("smallVideo", null);
+        emitter.emit("smallQuestion", null);
+        window.electron.showWindow(true);
+        updateCurrentWindow(data);
+    };
+
     onMounted(() => {
-        window.electron.ipcRenderer.on("openWindow", (_, data) => {
-            if (tabIndex.value === ClassroomType.Classes) {
-                tabIndex.value = ClassroomType.WindowClasses;
-            }
-            if (updateShowClassArrangement) {
-                updateShowClassArrangement(false);
-            }
-            emitter.emit("smallVideo", null);
-            emitter.emit("smallQuestion", null);
-            window.electron.showWindow(true);
-            updateCurrentWindow(data);
-        });
+        window.electron.ipcRenderer.removeListener("openWindow", openWindow);
+        window.electron.ipcRenderer.on("openWindow", openWindow);
+    });
+
+    onActivated(() => {
+        window.electron.ipcRenderer.removeListener("openWindow", openWindow);
+        window.electron.ipcRenderer.on("openWindow", openWindow);
+    });
+
+    onDeactivated(() => {
+        window.electron.ipcRenderer.removeListener("openWindow", openWindow);
     });
 
     onUnmounted(() => {
-        window.electron.ipcRenderer.removeAllListeners("openWindow");
+        window.electron.ipcRenderer.removeListener("openWindow", openWindow);
     });
 };
