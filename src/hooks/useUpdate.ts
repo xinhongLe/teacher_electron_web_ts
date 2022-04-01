01744596838fe8002cf86f0dd471f5a98e77297d
songwenlin <15706218382@163.com>
2022-04-01 11:43:39 +0800

    --task=1001936 --user=宋文林 备教端问题修改 https://www.tapd.cn/38241030/s/1121056 更新插件、版本号，增加更新内容

---
+++ src/hooks/useUpdate.ts
@@ -0,0 +1,44 @@
+import { ref } from "vue";
+
+interface IVersion {
+    version: string,
+    force: boolean,
+    desc: string[],
+}
+
+export default () => {
+    const updateVisible = ref(false);
+    const downloadPercent = ref(0);
+    const newVersionView = ref(false);
+    const ifShowCancelButton = ref(true);
+    const showUpdateInfo = ref<string[]>([]);
+
+    const getUpdateJson = () => {
+        const url = "https://app-v.oss-cn-shanghai.aliyuncs.com/teacherElectron/build/update.json";
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                showUpdateData(xhr.response);
+            }
+        };
+        xhr.send();
+    };
+
+    const showUpdateData = (res: string) => {
+        const data:IVersion[] = res ? JSON.parse(res) : [];
+        const version = (window as any).electron.getVersion();
+        const upVersionList = data.filter((item:IVersion) => item.version >= version);
+        const mustUpdate = upVersionList.find((item:IVersion) => item.force);
+        if (mustUpdate) ifShowCancelButton.value = false;
+        if (upVersionList.length > 0) showUpdateInfo.value = upVersionList[upVersionList.length - 1]?.desc;
+    };
+    return {
+        updateVisible,
+        downloadPercent,
+        newVersionView,
+        ifShowCancelButton,
+        showUpdateInfo,
+        getUpdateJson
+    };
+};
