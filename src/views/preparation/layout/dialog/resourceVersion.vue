85c6b44227295f93e8afc0598848d6cdbe1180c8
moneyinto <woyaoqianjin@gmail.com>
2022-07-20 16:34:52 +0800

    版本记录文件下载 --task=1003581 --user=钱进 资源中心、备教端 整改 https://www.tapd.cn/38241030/s/1214707

--- src/views/preparation/layout/dialog/resourceVersion.vue
+++ src/views/preparation/layout/dialog/resourceVersion.vue
@@ -12,7 +12,7 @@
             <el-table-column width="120px" prop="name" label="编辑人员" />
             <el-table-column prop="fileName" label="资源">
                 <template #default="scope">
-                    <el-button type="text">
+                    <el-button type="text" @click="download(scope.row.file)">
                         <div class="file-text">{{scope.row.fileName}}</div>
                     </el-button>
                 </template>
@@ -25,6 +25,20 @@
 import { getResourceHistory } from "@/api/resource";
 import moment from "moment";
 import { defineComponent, ref, watch } from "vue";
+import loading from "@/components/loading";
+import { getOssUrl } from "@/utils/oss";
+interface IFile {
+    Id: string;
+    Name: string;
+    FileName: string;
+    Size: number;
+    FileBucket: string;
+    FilePath: string;
+    FileExtention: string;
+    FileMD5: string;
+    OldFileID: string;
+}
+
 interface IHistroy {
     time: string;
     name: string;
@@ -76,9 +90,47 @@ export default defineComponent({
             }
         });
 
+        const download = async (file: IFile) => {
+            if (file) {
+				loading.show();
+                const url = await getOssUrl(`${file.FilePath}/${file.FileMD5}.${file.FileExtention}`, file.FileBucket);
+                getBlob(url, function(blob: any) {
+                    saveAs(blob, file.FileName);
+                });
+            }
+        };
+
+        const getBlob = (url: string, cb: any) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.responseType = "blob";
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    cb(xhr.response);
+                }
+            };
+            xhr.send();
+        };
+
+        const saveAs = (blob: any, name: string) => {
+            const link = document.createElement("a");
+            const body = document.querySelectorAll("body");
+            link.href = window.URL.createObjectURL(blob);
+            link.download = name;
+            link.style.display = "none";
+            body[0].appendChild(link);
+            link.click();
+            body[0].removeChild(link);
+            window.URL.revokeObjectURL(link.href);
+            setTimeout(() => {
+                loading.destroy();
+            }, 500);
+        };
+
         return {
             close,
-            tableData
+            tableData,
+            download
         };
     }
 });
