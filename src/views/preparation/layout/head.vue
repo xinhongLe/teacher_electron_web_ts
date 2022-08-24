96410d684c80794a10a02868e12e8907372b7859
moneyinto <woyaoqianjin@gmail.com>
2022-08-24 11:32:59 +0800

    更多设置调整 备课包记录检索调整 --task=1003228 --user=钱进 备教端项目优化调整任... https://www.tapd.cn/38241030/s/1242947

--- src/views/preparation/layout/head.vue
+++ src/views/preparation/layout/head.vue
@@ -785,7 +785,7 @@ export default defineComponent({
 			getCartOptionList({
 				lessonId: course.value.lessonId,
 				startTime: dateRange.value[0] ? moment(dateRange.value[0]).format("YYYY-MM-DD 00:00:00") : "",
-				endTime: dateRange.value[1] ? moment(dateRange.value[1]).format("YYYY-MM-DD 00:00:00") : "",
+				endTime: dateRange.value[1] ? moment(dateRange.value[1]).format("YYYY-MM-DD 23:59:59") : "",
 				paper: {
 					pageNumber: pageNumber.value,
 					pageSize: 10
