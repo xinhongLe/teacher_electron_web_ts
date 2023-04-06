31a0f1d7ab10f0ee96e01e2a423d61023bbdcffd
gufei <gufei@lyx-edu.com>
2023-04-06 11:22:23 +0800

    解决没数据问题

--- src/views/composition/components/list.vue
+++ src/views/composition/components/list.vue
@@ -55,7 +55,8 @@
                     <div class="button" v-if="item.Status === 5" @click="reCorrection(item)">重新批改</div>
                     <div class="button" v-if="item.Status === 2" @click="checkOrigin(item)">检查原文</div>
                     <div class="button" v-if="item.Status === 3 || item.Status === 6" @click="showReport(item)">查看报告</div>
-                    <div class="button" v-if="item.Status === 3 || item.Status === 4 || item.Status === 5 || item.Status === 6"
+                    <div class="button"
+                        v-if="item.Status === 3 || item.Status === 4 || item.Status === 5 || item.Status === 6"
                         @click="showArticle(item)">查看原文</div>
                 </div>
             </div>
@@ -137,8 +138,12 @@ const { gradeList, Title, total, grade, stuList } = toRefs(state)
 const tabList = ref<any>([])
 const tabChange = (name: any) => {
     state.page.PageNumber = 1
-    PaginationRef.value.page = 1
     state.tabName = name
+    nextTick(() => {
+        if(PaginationRef.value){
+            PaginationRef.value.page = 1
+        }
+    })
     getComList()
 }
 
