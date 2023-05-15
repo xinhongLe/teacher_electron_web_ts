3eafd4276bdeab769477578e1ad6832498a16952
DESKTOP-G4OO2VA\admin <1246450395@qq.com>
2023-05-15 17:23:13 +0800

    pbl入口优化

--- src/views/pblStudy/index.vue
+++ src/views/pblStudy/index.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="container">
-        <webview v-if="isElectron" class="iframe" :src="url"></webview>
+        <!--        <webview v-if="isElectron" class="iframe" :src="url"></webview>-->
         <iframe
             :src="url"
             sandbox="allow-forms allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"
             class="iframe"
-            v-else
         />
+        <!--        v-else-->
+
     </div>
 </template>
 
 <script lang="ts">
 import {get, STORAGE_TYPES} from "@/utils/storage";
-import {defineComponent} from "vue";
+import {defineComponent, onActivated, ref} from "vue";
 import {PBL_WEB} from "@/config";
 import isElectron from "is-electron";
 // ts
@@ -22,16 +23,21 @@ export default defineComponent({
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         window.onmessage = (e: any, d: any) => {
-            // console.log(e);
+            console.log(e);
             if (e.data.url && e.data.download) {
                 location.href = e.data.url;
             }
         };
-        //token 令牌
-        const token = get(STORAGE_TYPES.SET_TOKEN);
-        //webview地址
-        const url = `${PBL_WEB}/home?token=${token}`;
-        // console.log('url----', url);
+
+        const url = ref("");
+        onActivated(() => {
+            //token 令牌
+            const token = get(STORAGE_TYPES.SET_TOKEN);
+            //webview地址
+            url.value = `${PBL_WEB}/home?token=${token}`;
+            console.log('urlurlurlurlurl', url.value)
+            console.log('token', token)
+        })
         return {
             url,
             isElectron: isElectron(),
