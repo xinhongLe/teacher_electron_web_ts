04b8b7fa28b76b3dbdcbc28b3c108fed9ccd6969
DESKTOP-G4OO2VA\admin <1246450395@qq.com>
2023-05-18 15:09:42 +0800

    答题器 统计序号添加

--- electron/suspension.ts
+++ electron/suspension.ts
@@ -302,7 +302,7 @@ function createAnswerMachineWindow(allStudentList: []) {
         alwaysOnTop: true,
         maximizable: false,
     });
-    // answerMachineWin.webContents.openDevTools();
+    answerMachineWin.webContents.openDevTools();
 
     answerMachineWin.on("ready-to-show", () => {
         answerMachineWin && answerMachineWin.show();
