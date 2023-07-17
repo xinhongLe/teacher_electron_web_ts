e1065e0544ed53afc071fde6a62254ffbe9192dc
DESKTOP-G4OO2VA\admin <1246450395@qq.com>
2023-07-17 17:31:12 +0800

    工具栏优化，黑板优化

--- src/views/resourceCenter/index.vue
+++ src/views/resourceCenter/index.vue
@@ -12,19 +12,17 @@
 
 <script lang="ts">
 import {get, STORAGE_TYPES} from "@/utils/storage";
-import {defineComponent, onActivated, onDeactivated, computed} from "vue";
+import {defineComponent, onActivated, onDeactivated} from "vue";
 import {RESOURCE_WEB, systemId} from "@/config";
 import {IYunInfo} from "@/types/login";
 import usePageEvent from "@/hooks/usePageEvent";
 import isElectron from "is-electron";
 import {useRoute} from "vue-router";
-import {useStore} from "@/store";
 
 // ts
 export default defineComponent({
     name: "assessmentCenter",
     setup() {
-        const store = useStore();
         const {createBuryingPointFn} = usePageEvent("资源中心", true);
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
@@ -34,44 +32,42 @@ export default defineComponent({
                 location.href = e.data.url;
             }
         };
-        // token 令牌
+        //token 令牌
         const token = get(STORAGE_TYPES.SET_TOKEN);
-        // userInfo 当前登录用户信息
-        // const userInfo = get(STORAGE_TYPES.USER_INFO);
-        // 云平台信息
+        //userInfo 当前登录用户信息
+        const userInfo = get(STORAGE_TYPES.USER_INFO);
+        //云平台信息
         const yunInfo: IYunInfo = get(STORAGE_TYPES.YUN_INFO);
 
         // console.log('user-info', userInfo);
         // console.log('yunInfo', yunInfo);
 
-        // userId 用户id
-        const userId = computed(() => store.state.userInfo.userCenterUserID);
-        // orgId 部门id
+        //userId 用户id
+        const userId = userInfo.UserCenterUserID;
+        //orgId 部门id
         const orgID = yunInfo.OrgId;
-        // secretKey 密钥
+        //secretKey 密钥
         const secretKey = yunInfo.SecretKey;
-        // userType  用户类型：3.管理员 2.教师 1.学生 0.访客
+        //userType  用户类型：3.管理员 2.教师 1.学生 0.访客
         const userType = 2;
-        // schoolId 学校id
-        const schoolId = computed(() => store.state.userInfo.schoolId);
-        // schoolName 学校名称
-        const schoolName = computed(() => store.state.userInfo.schoolName);
-        // platType 平台码
+        //schoolId 学校id
+        const schoolId = userInfo.Schools[0].UserCenterSchoolID;
+        //schoolName 学校名称
+        const schoolName = userInfo.Schools[0].Name;
+        //platType 平台码
         const platType = systemId;
 
         const route = useRoute();
         const platformId = route.params.platformId;
 
-        // webview地址
-        const url = computed(() => {
-            return `${RESOURCE_WEB}/#/resource?TOKEN=${token}&USERID=${userId.value}&ORGID=${orgID}&SECRETKEY=${secretKey}&USERTYPE=${userType}&SCHOOLID=${schoolId.value}&SCHOOLNAME=${schoolName.value}&PLATTYPE=${platType}&platformId=${platformId}`
-        });
+        //webview地址
+        const url = `${RESOURCE_WEB}/#/resource?TOKEN=${token}&USERID=${userId}&ORGID=${orgID}&SECRETKEY=${secretKey}&USERTYPE=${userType}&SCHOOLID=${schoolId}&SCHOOLNAME=${schoolName}&PLATTYPE=${platType}&platformId=${platformId}`;
         // console.log('url----', url);
         return {
             url,
-            isElectron: isElectron()
+            isElectron: isElectron(),
         };
-    }
+    },
 });
 </script>
 
