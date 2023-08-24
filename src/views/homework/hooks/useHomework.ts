d02a4889f9ba02c82facb097ba82637c19e6234a
DESKTOP-G4OO2VA\admin <1246450395@qq.com>
2023-08-24 11:35:09 +0800

    布置作业 优化

--- src/views/homework/hooks/useHomework.ts
+++ src/views/homework/hooks/useHomework.ts
@@ -76,7 +76,7 @@ export default () => {
 
     const classList: any = computed(() => store.state.userInfo.classList);
 
-    const initData = async () => {
+    const initData = async (type?: number) => {
         const userInfo = get(STORAGE_TYPES.USER_INFO);
         const subData = await GetMySubjectByOrgId();
         if (subData.success && subData.resultCode === 200) {
@@ -101,7 +101,8 @@ export default () => {
     }, {deep: true});
     watch(() => store.state.userInfo.currentSelectClass, (val: any) => {
         if (!val.ClassAixueshiId) return;
-        initData()
+        selectClassId.value = val.ClassAixueshiId
+        // initData(0)
     }, {deep: true})
 
     onActivated(() => {
